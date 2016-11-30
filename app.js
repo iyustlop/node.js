085a9e1814210a9e5fdd54cb17143429a623fb5c
iyustlop <iyustlop@gmail.com>
2016-12-01 00:21:52 +0100

    Tutorial 5 debuging. Creation a response that implement Content-Lenght

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var http = require('http');
 
-var app = express();
+http.createServer(function(req,res){
+    var body = 'this is the body of response';
+    var content_length = body.length;
+    res.writeHead(200 , {
+        'Content-Type' : 'text/plain',
+        'Content-Length' : content_length
+    });
+    res.end( body);
+}).listen(3000);
 
-app.get("/",function(req,res){
+console.log("server is running on http://127.0.0.1:3000/");
 
-});
 
-app.listen(8080);
+
