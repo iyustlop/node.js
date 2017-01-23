52d8c19d79e1e3ef5248a12eeb93a97a485052f1
iyustlop <iyustlop@gmail.com>
2017-01-23 20:26:13 +0100

    unit 13: server a stream plain text.

---
+++ stream.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var http = require('http');
+
+
+var writeStream = fs.createWriteStream('write_file.txt')
+
+http.createServer(function(req,res){
+
+  res.writeHead(200,{'Content-Type': 'text/plain'});
+  var readStream = fs.createReadStream('texto.txt', 'utf8');
+  readStream.pipe(res);
+  
+}).listen(3000);
+
+console.log("Server is running in http://127.0.0.1:3000/");
