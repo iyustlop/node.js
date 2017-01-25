bbb51fe2980a0d0e73a72e8b3fc15a8db25b049a
iyustlop <iyustlop@gmail.com>
2017-01-25 20:09:05 +0100

    unit 16: basic routing

--- json.js
+++ json.js
@@ -3,7 +3,11 @@ var http = require('http');
 
 
 http.createServer(function(req,res){
-
+console.log('requested url : ' + req.url);
+if (req.url === '/'){
+  res.writeHead(200,{'Content-Type': 'text/html'});
+  fs.createReadStream('index.html').pipe(res);
+} else if (req.url === '/api'){
   res.writeHead(200,{'Content-Type': 'application/json'});
   var jsonObj = {
     name : 'ignacio',
@@ -11,6 +15,7 @@ http.createServer(function(req,res){
     age : 40
   };
   res.end(JSON.stringify(jsonObj));
+}
 }).listen(3000);
 
 console.log("Server is running in http://127.0.0.1:3000/");
