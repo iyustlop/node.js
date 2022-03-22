ff9a5fbdccdd72ddd04e7f6742e66df58d6c86e0
iyustlop <iyustlop@gmail.com>
2022-03-22 21:37:50 +0100

    Save in DB and Get from db

---
+++ backend/controllers/tracks.js
@@ -0,0 +1,51 @@
+const { tracksModel } = require("../models")
+
+
+/**
+ * obtener lista de la DB
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getItems = async (req, res) => {
+    const data = await tracksModel.find({})
+    res.send({data})
+};
+
+/**
+ * Obtener detalle
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getItem = (req, res) => {
+    const data = ["hola", "solo"]
+    
+    res.send({data})
+};
+
+/**
+ * Insertar un registro
+ * @param {*} req 
+ * @param {*} res 
+ */
+const createItem = async (req, res) => {
+    const { body }  = req
+    console.log(body)
+    const data = await tracksModel.create(body)
+    res.send({data})
+};
+
+/**
+ * eliminar un registro
+ * @param {*} req 
+ * @param {*} res 
+ */
+const deleteItem = (req, res) => {};
+
+/**
+ * Actualizar un registro
+ * @param {*} req 
+ * @param {*} res 
+ */
+const updateItems = (req, res) => {};
+
+module.exports = { getItems, getItem, createItem, deleteItem, updateItems }
--- backend/routes/tracks.js
+++ backend/routes/tracks.js
@@ -1,12 +1,9 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const { getItem, getItems, createItem } = require("../controllers/tracks");
+const router = express.Router();
 
-router.get("/", (req, res) => {
-    
-    const data = ["hola", "mundo"]
-    
-    res.send({data})
-})
-
+router.get("/", getItems);
+router.get("/:id", getItem);
+router.post("/", createItem)
 
 module.exports = router
