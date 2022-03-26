cb23965609b82b5026a8cc766bb28cb8dac505d1
iyustlop <iyustlop@gmail.com>
2022-03-26 14:38:35 +0100

    added middleware to check header

--- backend/routes/tracks.js
+++ backend/routes/tracks.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const customHeader = require("../middleware/customHeader")
 const { validatorCreateItem } = require("../validators/tracks")
 const { getItem, getItems, createItem } = require("../controllers/tracks");
 
 router.get("/", getItems);
 router.get("/:id", getItem);
-router.post("/", validatorCreateItem, createItem);
+router.post("/", validatorCreateItem, customHeader, createItem);
 
 module.exports = router
