f4d78bc48aba3f498da101a1e9aa6bdccdf38e76
Ignacio Yuste Lopez <iyustlop@gmail.com>
2022-10-13 22:41:02 +0200

    changed config to point mongodb atlas

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
 
 const dbConnect = () =>  {
-    const DB_URI = process.env.DB_URI
+    const DB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DATABASE}`
+    console.log(DB_URI)
     mongoose.connect(DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
